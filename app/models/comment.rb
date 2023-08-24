325d9bc82b39626949d0e8af1e9f362729f32071
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-25 01:55:03 +0300

    Add method to change comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post
+
+  after_create :comments_increment
+  after_destroy :comments_decrement
+
+  def comments_increment
+    post.increment!(:comments_counter)
+  end
+  def comments_decrement
+    post.decrement!(:comments_counter)
+  end
 end
