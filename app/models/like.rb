1fff02b9978a49ecdda681cf4b97a2ca5a31eedc
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-25 02:02:56 +0300

    Add custom methods on like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user, foreign_key: 'author_id'
+
+  after_create :likes_increment
+  after_destroy :likes_decrement
+
+  def likes_increment
+    post.increment!(:likesCounter)
+  end
+  def likes_decrement
+    post.decrement!(:likesCounter)
+  end
 end
