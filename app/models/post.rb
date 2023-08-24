f34497d6df7edcd7ea2c5b373dc313c458192bcd
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-25 01:41:30 +0300

    Add custom methods in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  after_create :posts_increment
+  after_destroy :posts_decrement
+
+  def posts_increment
+    user.increment!(:post_counter)
+  end
+  def posts_decrement
+    user.decrement!(:post_counter)
+  end
+  def recent_comments
+    comments.order(created_at: :DESC).limit(5)
+  end
 end
