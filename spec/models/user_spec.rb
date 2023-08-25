eb7a57ce31ad40a33d03b1320ccf368ed5258c89
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-26 02:37:35 +0300

    Add specs for #recent_posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,28 @@ RSpec.describe User, type: :model do
 
     before {subject.save}
 
-    it 'Name should be present' do
-        subject.name = nil 
-        expect(subject).to_not be_valid
+    describe 'validations' do
+        it 'Name should be present' do
+            subject.name = nil 
+            expect(subject).to_not be_valid
+        end
+        it 'posts_counter should be an integer' do
+            subject.posts_counter = 1.1
+            expect(subject).to_not be_valid
+        end
+        it 'posts_counter should be equal to or greater than 0' do
+            subject.posts_counter = -1
+            expect(subject).to_not be_valid
+        end
     end
-    it 'posts_counter should be an integer' do
-        subject.posts_counter = 1.1
-        expect(subject).to_not be_valid
-    end
-    it 'posts_counter should be equal to or greater than 0' do
-        subject.posts_counter = -1
-        expect(subject).to_not be_valid
-    end
+    describe '#recent_posts' do
+        it 'Returns 3 of the most recent posts' do
+            first_post = subject.posts.create(title: 'first post', text: 'first content')
+            second_post = subject.posts.create(title: 'second post', text: 'second content')
+            third_post = subject.posts.create(title: 'third post', text: 'third content')
+            fourth_post = subject.posts.create(title: 'fourth post', text: 'fourth content')
+
+            expect(subject.recent_posts).to eq([fourth_post, third_post, second_post])
+        end
   end
+end
