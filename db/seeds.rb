433607cfb36fb78f294bc06e741f879d6091e287
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-31 00:47:30 +0300

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create([{id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.'}, 
+    {id: 2, name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.'}])
+Post.create([{author_id: 1, title: 'Hello', text: 'This is my first post'}, {author_id: 1, title: 'Hello', text: 'This is my first post'}])
+Comment.create([{post_id: 1, author_id: 2, text: 'Hi Tom!' }, {post_id: 1, author_id: 2, text: 'Hi Tom!' }])
