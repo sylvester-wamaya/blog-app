bd18c2e04516793392627926b4443ae31a856576
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-31 01:32:57 +0300

    Add html content view on get users page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create([{id: 1, name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.'}, 
-    {id: 2, name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.'}])
+User.create([{id: 1, name: 'Tom', photo: 'https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=1024x1024&w=is&k=20&c=5OK7djfD3cnNmQ-DR0iQzF-vmA-iTNN1TbuEyCG1DfA=', bio: 'Teacher from Mexico.'}, 
+    {id: 2, name: 'Lilly', photo: 'https://media.istockphoto.com/id/1388253782/photo/positive-successful-millennial-business-professional-man-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=v0FzN5RD19wlMvrkpUE6QKHaFTt5rlDSqoUV1vrFbN4=', bio: 'Teacher from Poland.'}])
 Post.create([{author_id: 1, title: 'Hello', text: 'This is my first post'}, {author_id: 1, title: 'Hello', text: 'This is my first post'}])
 Comment.create([{post_id: 1, author_id: 2, text: 'Hi Tom!' }, {post_id: 1, author_id: 2, text: 'Hi Tom!' }])
