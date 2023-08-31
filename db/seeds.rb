81832a8ace958afa1ffef144853a500f239a8653
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-31 18:28:31 +0300

    Seed likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ User.create([{id: 1, name: 'Tom', photo: 'https://media.istockphoto.com/id/13864
     {id: 2, name: 'Lilly', photo: 'https://media.istockphoto.com/id/1388253782/photo/positive-successful-millennial-business-professional-man-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=v0FzN5RD19wlMvrkpUE6QKHaFTt5rlDSqoUV1vrFbN4=', bio: 'Teacher from Poland.'}])
 Post.create([{author_id: 1, title: 'Hello', text: 'This is my first post'}, {author_id: 1, title: 'Hello', text: 'This is my first post'}])
 Comment.create([{post_id: 1, author_id: 2, text: 'Hi Tom!' }, {post_id: 1, author_id: 2, text: 'Hi Tom!' }])
+Like.create([{author_id:2, post_id:1},{author_id:2, post_id:1},{author_id:2, post_id:2}])
