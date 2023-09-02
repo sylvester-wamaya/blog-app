49519c00447e1880030045429a891d2eed71852e
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-02 07:01:02 +0300

    Add create and new methods

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.new(comment_params)
+        @comment.user = current_user
+    
+        if @comment.save
+          redirect_to user_post_path(@post.user, @post), notice: 'Comment added successfully'
+        else
+            redirect_to user_post_path(@post.user, @post), alert: 'Failed to save comment'
+        end
+      end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:text)
+      end
+    
+end
