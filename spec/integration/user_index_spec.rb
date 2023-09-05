5192172106d11fb59f72a542194d5251a0368d22
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-09-05 21:39:59 +0300

    Test the display of user photos on user page

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -13,5 +13,10 @@ RSpec.describe 'User index page', type: :feature do
             expect(page).to have_content('Tom')
             expect(page).to have_content('Lilly')
         end
+        it 'Should display user photos' do
+            expect(page).to have_css("img[src*='https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=1024x1024&w=is&k=20&c=5OK7djfD3cnNmQ-DR0iQzF-vmA-iTNN1TbuEyCG1DfA=']", visible: :visible)
+            expect(page).to have_css("img[src*='https://media.istockphoto.com/id/1388253782/photo/positive-successful-millennial-business-professional-man-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=v0FzN5RD19wlMvrkpUE6QKHaFTt5rlDSqoUV1vrFbN4=']", visible: :visible)
+
+        end
     end
 end
